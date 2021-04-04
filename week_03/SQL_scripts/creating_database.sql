34955fc76acdbad9dce948056d889352e4224ebc
agalvezcorell <agalvezcorell@gmail.com>
2021-04-04 15:28:23 +0200

    preparing sql

---
+++ week_03/SQL_scripts/creating_database.sql
@@ -0,0 +1,42 @@
+/* Estos son los requisitos de nuestra base de datos de ejemplo:
+La Escuela Internacional de Idiomas es una escuela de formación lingüística.
+Ofrece clases de idiomas para clientes corporativos, que pueden impartirse en la Escuela o en las oficinas del cliente, según prefieran. 
+La Escuela emplea a profesores, cada uno de los cuales puede impartir múltiples cursos.
+La Escuela tiene clientes, cada uno de los cuales puede ofrecer múltiples cursos a través de la escuela. 
+Los clientes ofrecen cursos a sus empleados, que tienen la opción de participar.
+Cada curso es ofrecido por un cliente. 
+Cada curso tiene un profesor en todo momento.
+Los participantes en los cursos son empleados de las empresas clientes, es decir, trabajan para ellas. 
+Cada participante puede ser empleado de una empresa a la vez. 
+Los participantes pueden estar inscritos en más de un curso.
+
+
+see: https://towardsdatascience.com/designing-a-relational-database-and-creating-an-entity-relationship-diagram-89c1c19320b2
+*/ 
+
+#############################
+#      CREATE THE DB        #
+#############################
+# doomsday!
+
+
+
+#############################
+#      CREATE THE TABLES    #
+#############################
+-- STEP 1 teacher
+
+
+###################################
+#      CREATE THE RELATIONHIPS    #
+###################################
+
+
+###################################
+#      POPULATE THE DATABASE      #
+###################################
+
+
+###################################
+#           PLAY AROUND           #
+###################################
